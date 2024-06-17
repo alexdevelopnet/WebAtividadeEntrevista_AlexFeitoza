73196585ec27121630801c8a3ee4a2de8db8c0c8
alexdevelopnet <alexfnet@gmail.com>
2024-06-17 17:29:20 -0300

    Add project files.

---
+++ FI.WebAtividadeEntrevista/Models/BeneficiarioModel.cs
@@ -0,0 +1,34 @@
+﻿using FI.AtividadeEntrevista.DML;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAtividadeEntrevista.Models
+{
+    /// <summary>
+    /// Classe de Modelo de Beneficiario
+    /// </summary>
+    public class BeneficiarioModel
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Nome
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Cpf
+        /// </summary>
+        [RegularExpression(@"\d{3}\.\d{3}\.\d{3}-\d{2}", ErrorMessage = "Digite um e-cpf válido")]
+        public string Cpf { get; set; }
+        /// <summary>
+        /// Cpf
+        /// </summary>
+        public string IdCliente { get; set; }
+        public IEnumerable<Beneficiario> Beneficiarios { get; set; } = new List<Beneficiario>();
+
+    }
+}
